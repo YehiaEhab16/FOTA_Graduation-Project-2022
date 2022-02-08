d691f43c4409593b2dffeddda8f0ad68d243f3d2
unknown <yehiaahmed2000000@gmail.com>
2022-02-08 16:51:01 +0200

    Comm.py

--- RPI/GUI/Script/Comm.py
+++ RPI/GUI/Script/Comm.py
@@ -1,9 +1,40 @@
 # Functions for communication protocol
-
+import serial 
+import threading
+import time
 # Receive Function
-def Get():
-	return 0
+global data 
+data = '1' 
+class Read_thread (threading.Thread):
+	def _init__(self):
+		threading.Thread.__init__(self)
 
 
-def Send():
-	pass
+	def run (self):
+		Serial_Port = serial.Serial ("/dev/ttyS0") 
+		Serial_Port.baudrate = 9600
+		global data 
+		while True :
+			data = Serial_Port.read(1)
+			#print (data)
+			time.sleep(4)
+			
+
+
+
+def Stop_Read (Read_thread):
+	Serial_Port.close()
+	Read_thread.kill()
+
+def Start_Read (Read_thread):
+	Read_thread.start()
+
+def Read_Data():
+	global data 
+	return data
+	
+def Send(data):
+	Serial_Port = serial.Serial ("/dev/ttyS0",9600)   
+	Serial_Port.baudrate = 9600  
+	Serial_Port.write(data)
+	Serial_Port.close()
