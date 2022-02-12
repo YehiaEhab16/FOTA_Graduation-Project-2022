770cb2009bb5cc84f7e1b232441b285a9f7d96e3
Esraa Elbakly <73258793+Esraa-Elbakly@users.noreply.github.com>
2022-02-13 01:28:45 +0200

    Add files via upload

--- ARM/1-MCAL/06-USART/main.c
+++ ARM/1-MCAL/06-USART/main.c
@@ -1,81 +1,40 @@
-#include
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "PORT_interface.h"
+#include "PORT_register.h"
+#include "delay.h"
+#include "RCC_interface.h"
+#include "GPIO_interface.h"
+#include "USART_interface.h"
+#include "USART_private.h"
+#include "USART_register.h"
 
 
+int main(){
 
 
+  /*Enable USART RCC*/
+  RCC_voidInit();
+  PORT_voidInit();
+  USART_voidInit(USART1);
 
-void ahmed (void)
-{
-	/*Pin A2 High			*/
-	GPIO_voidSetPinValue(GPIOA , PIN2 , HIGH );
-	/*Delay for 1 Sec		*/ 
-	STK_voidSetBusyWait( 1000000 );
-	
-	/*Pin A2 Low			*/
-	GPIO_voidSetPinValue(GPIOA , PIN2 , LOW );
-	/*Delay for 1 Sec		*/ 
-	STK_voidSetBusyWait( 1000000 );
-	
+  /*TX push pull alternative function*/
+  GPIO_u8SetPinMode(USART1_PORT , USART1_TX_PIN , OUTPUT_SPEED_2MHZ_AFPP);
+  GPIO_u8SetPinMode(GPIO_PORTB , GPIO_PIN_1 , OUTPUT_SPEED_2MHZ_PP);
+  GPIO_u8SetPinMode(GPIO_PORTC , GPIO_PIN_13 , OUTPUT_SPEED_2MHZ_PP);
+  GPIO_u8SetPinMode(GPIO_PORTA , GPIO_PIN_10 , INPUT_FLOATING);
+  GPIO_u8SetPinValue(GPIO_PORTB , GPIO_PIN_1 , GPIO_PIN_LOW);
+  GPIO_u8SetPinValue(GPIO_PORTB , GPIO_PIN_3 , GPIO_PIN_LOW);
+
+  //u8 data = 0 ;
+  //USART_voidTransmittChar('A');
+  while (1){
+
+    USART_voidTransmitChar(USART1_BASE, "Mayar Alaa Esraa\r\n");
+    GPIO_u8SetPinValue(GPIO_PORTC , GPIO_PIN_13 , GPIO_PIN_HIGH);
+    delay(200000);
+    USART_voidTransmitChar(USART1_BASE, " ");
+    delay(500000);
+  }
+  return 0;
 }
-void atia (void)
-{
-	/*Pin A2 High			*/
-	GPIO_voidSetPinValue(GPIOA , PIN3 , HIGH );
-	/*Delay for 1 Sec		*/ 
-	STK_voidSetBusyWait( 1000000 );
-	
-	/*Pin A2 Low			*/
-	GPIO_voidSetPinValue(GPIOA , PIN3 , LOW );
-	/*Delay for 1 Sec		*/ 
-	STK_voidSetBusyWait( 1000000 );
-	
-}
-
-int main(void)
-{
-	/*RCC								*/
-	RCC_voidInitSysClock(void);
-	/*RCC AFIO							*/
-	RCC_voidEnablePeripheralClock(APB2 , 0);
-	
-	RCC_voidEnablePeripheralClock(APB2 , GPIOA);
-	EXTI_voidSetCallBack(ahmed);
-	EXTI_voidSetCallBack(atia);
-	
-	/**		GPIO Direction	**/
-	/*		A0 = input =====> PULL UP Resistor	*/
-	GPIO_voidSetPinDirection(GPIOA , PIN0 , INPUT_PULLUP_DOWN);
-	GPIO_voidSetPinValue(GPIOA , PIN0 ,HIGH );
-
-	GPIO_voidSetPinDirection(GPIOA , PIN1 , INPUT_PULLUP_DOWN);
-	GPIO_voidSetPinValue(GPIOA , PIN1 ,HIGH );
-	/**	A2 ======> OUTPUT  SPEED 2MHZ PUSH PULL 	*/
-	GPIO_voidSetPinDirection(GPIOA , PIN2 , OUTPUT_SPEED_2MHZ_PP);	
-	GPIO_voidSetPinDirection(GPIOA , PIN3 , OUTPUT_SPEED_2MHZ_PP);	
-		
-	EXTI_voidInit();
-	EXTI_voidSetSignalLatch(EXTI_LINE0 , FALLING_EDGE);
-	EXTI_voidSetSignalLatch(EXTI_LINE1 , FALLING_EDGE);
-		
-	NVIC_voidSetPriority(6 , 2);
-	NVIC_voidSetPriority(7 , 1);
-	NVIC_voidEnableInterrupt(6);
-	NVIC_voidEnableInterrupt(7);
-	//NVIC_voidSetPendingFlag(6);
-	
-	while(1);
-	
-	return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
