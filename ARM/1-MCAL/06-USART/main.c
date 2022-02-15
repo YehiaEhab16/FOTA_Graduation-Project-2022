bf837f6cd8421df4a20cae2e9d37faf4b28f2642
Esraa Elbakly <73258793+Esraa-Elbakly@users.noreply.github.com>
2022-02-15 02:18:59 +0200

    delay file&remove some unused includes

--- ARM/1-MCAL/06-USART/main.c
+++ ARM/1-MCAL/06-USART/main.c
@@ -1,13 +1,11 @@
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
 #include "PORT_interface.h"
-#include "PORT_register.h"
 #include "delay.h"
 #include "RCC_interface.h"
 #include "GPIO_interface.h"
 #include "USART_interface.h"
-#include "USART_private.h"
-#include "USART_register.h"
+
 
 
 int main(){
