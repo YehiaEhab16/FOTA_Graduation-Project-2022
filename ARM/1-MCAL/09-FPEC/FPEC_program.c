d4d946b4a24cb7e49c2a8b6ec106dd11352738c9
unknown <yehiaahmed2000000@gmail.com>
2022-03-07 05:25:15 +0200

    prepare FPEC driver

---
+++ ARM/1-MCAL/09-FPEC/FPEC_program.c
@@ -0,0 +1,65 @@
+/*******************************************************************************/
+/*******************************************************************************/
+/***********************   GRADUATION PROJECT : (FOTA)   ***********************/
+/***********************   Layer :MCAL                   ***********************/
+/***********************   SWC (DRIVER):FPEC 			 ***********************/
+/***********************   DATA : 7-3-2022  			 ***********************/
+/*******************************************************************************/
+/*******************************************************************************/
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "FPEC_interface.h"
+#include "FPEC_config.h"
+#include "FPEC_private.h"
+#include "FPEC_register.h"
+
+
+
+void FPEC_voidInit(void)
+{
+
+}
+
+u8 FPEC_voidEraseFlashArea(u8 Copy_u8StartPage , u8 Copy_u8EndPage )
+{
+	u8 Local_u8ErrorState =OK ;
+
+
+
+	return Local_u8ErrorState;
+}
+
+u8 FPEC_voidEraseFlash(void)
+{
+	u8 Local_u8ErrorState =OK ;
+
+
+
+	return Local_u8ErrorState;
+}
+
+u8 FPEC_voidFlashPageErase(u8 Copy_u8PageNumber)
+{
+	u8 Local_u8ErrorState =OK ;
+
+
+
+	return Local_u8ErrorState;
+}
+
+void FPEC_voidFlashWrite(u32 Copy_u32Address, u16* Copy_u16Data, u8 Copy_u8Length)
+{
+
+
+}
+
+
+
+
+
+
+
+
+
