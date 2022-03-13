02d67320f65db20ac5699db9d9ba6137c176a437
unknown <yehiaahmed2000000@gmail.com>
2022-03-13 03:07:02 +0200

    Modify BootLoader

--- ARM/4-APP/BOOTLOADER_APPLICATION/BOOTLOADER/src/PARSING_program.c
+++ ARM/4-APP/BOOTLOADER_APPLICATION/BOOTLOADER/src/PARSING_program.c
@@ -110,39 +110,3 @@ u8 AsciToHex(u8 Copy_u8Asci)
 }
 
 
-void ParseData(u8* Copy_u8BufData)
-{
-	u8 DigitLow,DigitHigh,CC,i;
-	u8 DataDigit0,DataDigit1,DataDigit2,DataDigit3;
-	u8 DataCounter = 0;
-
-	/* Get Character Count */
-	DigitHigh = AsciToHex (Copy_u8BufData[1]);
-	DigitLow  = AsciToHex (Copy_u8BufData[2]);
-	CC        = (DigitHigh<<4) | DigitLow ;
-
-	/* Get Address */
-	DataDigit0 = AsciToHex (Copy_u8BufData[3]);
-	DataDigit1 = AsciToHex (Copy_u8BufData[4]);
-	DataDigit2 = AsciToHex (Copy_u8BufData[5]);
-	DataDigit3 = AsciToHex (Copy_u8BufData[6]);
-
-	/* Clear Low Part of Address */
-	Address = Address & 0xFFFF0000;
-	Address = Address | (DataDigit3) | (DataDigit2 << 4) | (DataDigit1 << 8) | (DataDigit0<<12);
-
-	for (i=0;i<CC/2; i++)
-	{
-		DataDigit0 = AsciToHex (Copy_u8BufData[4*i+9 ]);
-		DataDigit1 = AsciToHex (Copy_u8BufData[4*i+10]);
-		DataDigit2 = AsciToHex (Copy_u8BufData[4*i+11]);
-		DataDigit3 = AsciToHex (Copy_u8BufData[4*i+12])	;
-
-		Data[DataCounter] = (DataDigit3 << 8) | (DataDigit2 << 12) | (DataDigit1) | (DataDigit0<<4);
-		DataCounter++;
-	}
-
-	FPEC_voidFlashWrite(Address,Data,CC/2);
-}
-
-
