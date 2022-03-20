b7074317f6ef19e5a8a2e0861c8657c1ba1f17ef
Esraa Elbakly <73258793+Esraa-Elbakly@users.noreply.github.com>
2022-03-20 04:04:55 +0200

    need a test

--- ARM/3-SERVICE/03-USART_S/USART_S_program.c
+++ ARM/3-SERVICE/03-USART_S/USART_S_program.c
@@ -9,8 +9,9 @@
 
 #include "STD_TYPES.h"
 
-#include "RCC_interface"
-#include "USART_interface"
+#include "RCC_interface.h"
+#include "PORT_interface.h"
+#include "USART_interface.h"
 
 #include "USART_S_interface.h"
 #include "USART_S_private.h"
@@ -18,10 +19,23 @@
 
 void USART_S_voidInit(void)
 {
-	
+		
+  RCC_voidInit();
+  RCC_voidEnableClock((u8)RCC_APB2,(u8) RCC_APB2_USART1_EN);
+  RCC_voidEnableClock((u8)RCC_APB2,(u8) RCC_APB2_IOPA_EN);
+  PORT_voidInit();
+  
+/*Option1 Select : 	1- USART1
+					2- USART2
+					3- USART3	
+					*/
+  USART_voidInit(USART1);
+  
 }
+
+
 
 void USART_S_voidDisable(void)
 {
-	
+	RCC_u8DisableClock ((u8)RCC_APB2);
 }
