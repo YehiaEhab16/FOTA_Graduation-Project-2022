4d517f26b1575c0d5898b5d6f36750db91019d3b
YehiaEhab16 <yehiaorfy3@gmail.com>
2022-03-21 19:04:56 +0200

    RTOS Initial Commit (Scheduler + RTOS_start)

--- ARM/4-RTOS/RTOS_private.h
+++ ARM/4-RTOS/RTOS_private.h
@@ -12,6 +12,24 @@
 #define RTOS_PRIVATE_H_
 
 
+struct RTOS_sTasks
+{
+	u32 RTOS_u32Periodicity;
+	u32 RTOS_u32FirstDelay;
+	void *RTOS_pvoid(void)=NULL;
+	u8 RTOS_u8Priority;
+	u8 RTOS_u8TaskState;
+};
+
+//RTOS Task States
+#define RTOS_READY			0
+#define RTOS_SUSPENDED		0
+
+
+#define RTOS_NUM_TASKS	100
+
+#define RTOS_TICKTIME	5
+		
 static void Scheduler(void);
 
 
--- ARM/4-RTOS/RTOS_program.c
+++ ARM/4-RTOS/RTOS_program.c
@@ -15,16 +15,24 @@
 #include "RTOS_private.h"
 #include "RTOS_register.h"
 
+struct RTOS_sTasks[RTOS_NUM_TASKS] = {NULL};
+
 /*Scheduler Functions*/
 //Start Timer with specific tick time and Call Scheculer 
 void RTOS_voidStart(void)
 {
-	
+	STK_voidInit();
+	STK_u8DelayAsyPeriodic(RTOS_TICKTIME, &Scheduler);
 }
 //Task Scheduling according to specific algorithm
 static void Scheduler(void)
 {
-	
+	u8 Local_u8Counter=0;
+	for(Local_u8Counter=0;Local_u8Counter<=RTOS_NUM_TASKS;Local_u8Counter++)
+	{
+		if(RTOS_sTasks[Local_u8Counter].RTOS_pvoid!=NULL && RTOS_sTasks[Local_u8Counter].RTOS_u8TaskState==RTOS_READY)
+			RTOS_sTasks.RTOS_pvoid();
+	}
 }
 
 /*Tasks Handling Functions*/
