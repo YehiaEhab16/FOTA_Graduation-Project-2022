7d3f67294acd607041592f62fd93df64897248c0
Esraa Elbakly <73258793+Esraa-Elbakly@users.noreply.github.com>
2022-04-25 05:38:16 +0200

    simple prototype

---
+++ ARM/5-APP/APP ECU/Apps.c
@@ -0,0 +1,37 @@
+/*******************************************************************************/
+/*******************************************************************************/
+/***********************   GRADUATION PROJECT : (FOTA)   ***********************/
+/***********************   Layer :HAL                   ***********************/
+/***********************   DATA : 25/4/2022 			 ***********************/
+/*******************************************************************************/
+/*******************************************************************************/
+
+
+#include "Appsh"
+
+/*this function will 
+					Operate the tempreture sensor
+					Return a flag when the reading is over or below the normal values(no reading of garpage)
+					Return a message if the value has suddenly changed by a large absolute differance (the current and the previous value)					
+*/
+u8 TempSensor (){
+	
+}
+
+
+/*this function will 
+					Operate the motors			
+*/
+u8 Motors (){
+	
+}
+
+/*this function will 
+					Operate the ultrasonic sensor
+					Return a flag when the reading is over or below the normal values(no reading of garpage).
+					Return a message if the value has suddenly changed by a large absolute differance (the current and the previous value).
+					In the last case the fuction will operate a buzzer and flash.					
+*/
+u8 CollisionSystem (){
+
+}
--- ARM/5-APP/APP ECU/main.c
+++ ARM/5-APP/APP ECU/main.c
@@ -1,8 +1,62 @@
 /*******************************************************************************/
 /*******************************************************************************/
 /***********************   GRADUATION PROJECT : (FOTA)   ***********************/
-/***********************   App ECU Application         	 ***********************/
-/***********************   DATA : 17-3-2022  			 ***********************/
+/***********************   Layer :App ECU Application   ***********************/
+/***********************   DATA : 25/4/2022 			 ***********************/
 /*******************************************************************************/
 /*******************************************************************************/
 
+
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "RCC_interface.h"
+#include "GPIO_interface.h"
+#include "RTOS_interface.h"
+
+
+/*parameters for task creation where The formate will be like:
+			TASK_t Copy_TaskName_tInfo{Periodicity, FirstDelay ,&FunctionWillBeExecuted ,Priority1 ,TaskState}
+			The following is the information of the tasks where their types is defined in TASK_t
+
+  
+
+
+
+*/
+
+
+
+TASK_t Copy_CollisionTASK_tInfo  	 = {1000, 1000 , &CollisionSystem ,1 ,RTOS_READY};
+TASK_t Copy_TempSensorTASK_tInfo 	 = {1000, 1000 , &TempSensor      ,2 ,RTOS_READY};
+TASK_t Copy_MotorsTASK_tInfo     	 = {1000, 1000 , &Motors          ,3 ,RTOS_READY};
+TASK_t Copy_DiagTASK_tInfo       	 = {1000, 1000 , &Diagnostics     ,4 ,RTOS_READY};
+TASK_t Copy_RecCanTASK_tInfo 		 = {1000, 1000 , &CAN_voidReceive ,5 ,RTOS_READY};
+TASK_t Copy_TransmitCanTASK_tInfo 	 = {1000, 1000 , &CAN_u8Transmit  ,6 ,RTOS_READY};
+TASK_t Copy_UpdateTASK_tInfo       	 = {1000, 1000 , &updateSystem    ,7 ,RTOS_READY};
+
+
+
+
+int main(void)
+{
+
+
+	RTOS_u8CreateTask(& Copy_CollisionTASK_tInfo);
+	RTOS_u8CreateTask(& Copy_TempSensorTASK_tInfo);
+	RTOS_u8CreateTask(& Copy_MotorsTASK_tInfo);
+	RTOS_u8CreateTask(& Copy_DiagTASK_tInfo);
+	RTOS_u8CreateTask(& Copy_RecCanTASK_tInfo);
+	RTOS_u8CreateTask(& Copy_TransmitCanTASK_tInfo);
+	
+	
+	
+	Scheduler();
+	while(1){
+	
+	
+	}
+
+
+	return 0;
+}
