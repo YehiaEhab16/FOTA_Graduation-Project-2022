ca65397b6315c97df0de3972be914bfa909889b8
esraa-elbakly <saraalover588@gmail.com>
2022-05-26 14:59:41 +0200

    edit pins

--- ARM/2-HAL/07-DCMotor/DCMotor_interface.h
+++ ARM/2-HAL/07-DCMotor/DCMotor_interface.h
@@ -15,9 +15,7 @@
 #define  PWMPORT 		GPIO_PORTA  //for example
 #define  DR1_PIN        GPIO_PIN_0
 #define  DR2_PIN        GPIO_PIN_1
-#define  PWM_PIN		GPIO_PIN_2
-#define  ENCA 			GPIO_PIN_3 
-#define  ENCB 			GPIO_PIN_4 
+#define  ENC_PIN 		GPIO_PIN_3 
 #define  HIGH               1
 #define  LOW                0
 
@@ -25,7 +23,7 @@
 /**
 
  */
-void setMotor(int dir, int Copy_u16PWMValue);
+void setMotor(u8 dir);
 /**
 
  */
