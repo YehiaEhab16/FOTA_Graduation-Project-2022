b4f242370578c0d8df79991df0de9dc32c5b863f
YehiaEhab16 <yehiaorfy3@gmail.com>
2022-05-27 19:34:46 +0200

    Fixed Bug in App ECU

--- ARM/5-APP/APP ECU/main.c
+++ ARM/5-APP/APP ECU/main.c
@@ -7,7 +7,7 @@
 /*******************************************************************************/
 
 #include "../../6-Library/STD_TYPES.h"
-#include "SYS_interface"
+#include "../../3-SERVICE/02-SYS/SYS_interface.h"
 
 #include "../../4-freeRTOS/OS/org/Source/include/FreeRTOS.h"
 #include "../../4-freeRTOS/OS/org/Source/include/task.h"
@@ -18,16 +18,15 @@
 int main(void)
 {
 	//Initialization
-	SYS_voidAppInit(&Task_voidRecieveCANFunctionISR);
+	SYS_voidAppInit(&Task_voidCANRecieveISR);
 	
 	//Task Creation
 	xTaskCreate(Task_voidAlert, 	      "Task1", configMINIMAL_STACK_SIZE, NULL, 0, NULL);
 	xTaskCreate(Task_voidReadDirection,   "Task2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 	xTaskCreate(Task_voidReadTemperature, "Task3", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
 	xTaskCreate(Task_voidReadDistance,    "Task4", configMINIMAL_STACK_SIZE, NULL, 0, NULL);
-	xTaskCreate(Task_voidSystemCheck,     "Task5", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
 	xTaskCreate(Task_voidMoveVehicle,     "Task6", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-	xTaskCreate(Task_voidRecieveRequest,  "Task7", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+	xTaskCreate(Task_voidSystemCheck,     "Task5", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
 	xTaskCreate(Task_voidSendDiagnostics, "Task8", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
 	
 	//Start Scheduler
