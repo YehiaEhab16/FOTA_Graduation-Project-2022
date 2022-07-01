82fba5e450fccdf2e4e2b754fda503731c45dbca
yehia2000000 <yehiaahmed2000000@gmail.com>
2022-07-01 02:41:39 +0200

    DMA Driver

--- ARM/1-MCAL/14-DMA/DMA_program.c
+++ ARM/1-MCAL/14-DMA/DMA_program.c
@@ -18,114 +18,201 @@
 u8 DMA_u8ReadRegisterContent(DMA_t* Copy_DMA_tTransferConfig)
 {
 	u8 Local_u8ErrorState=OK;
-	u32 DMA_CCR, DMA_CNDTR, DMA_CPAR, DMA_CMAR;
+	u32* DMA_CCR;
+	u32*DMA_CNDTR;
+	u32*DMA_CPAR;
+	u32*DMA_CMAR;
 
-	if((Copy_DMA_tTransferConfig->DMA_u8PeripheralID >= DMA_PER_ADC1) &&(Copy_DMA_tTransferConfig->DMA_u8PeripheralID <= DMA_PER_TIMER4))
-	{
-		DMA_CCR   = DMA1_CCR1   + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET;
-		DMA_CNDTR = DMA1_CNDTR1 + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET;
-		DMA_CPAR  = DMA1_CPAR1  + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET;
-		DMA_CMAR  = DMA1_CMAR1  + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET;
-	}
-	else if((Copy_DMA_tTransferConfig->DMA_u8PeripheralID >= DMA_PER_ADC3) &&(Copy_DMA_tTransferConfig->DMA_u8PeripheralID <= DMA_PER_TIMER8))
-	{
-		DMA_CCR   = DMA2_CCR1   + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET;
-		DMA_CNDTR = DMA2_CNDTR1 + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET;
-		DMA_CPAR  = DMA2_CPAR1  + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET;
-		DMA_CMAR  = DMA2_CMAR1  + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET;
-	}
-	else 
-		Local_u8ErrorState=NOK;
+	//Disable DMA1
+	CLR_BIT(DMA1_CCR1,EN);
+	while (GET_BIT(DMA1_CCR1,EN) == 1) ;
+
+
+	DMA_CCR   = (u32*)(DMA1_CCR1   + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET);
+	DMA_CNDTR = (u32*)(DMA1_CNDTR1 + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET);
+	DMA_CPAR  = (u32*)(DMA1_CPAR1  + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET);
+	DMA_CMAR  = (u32*)(DMA1_CMAR1  + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET);
+
+
 	//Set Peripheral Address
 	*DMA_CPAR = Copy_DMA_tTransferConfig->DMA_u8PeripheralID;
 	//Set Memory Address 
 	*DMA_CMAR = DMA_MEMORY_ADDRESS;
 	//Number of data to be transferred 
 	*DMA_CNDTR = Copy_DMA_tTransferConfig->DMA_u8DataTransferNum;
+
+
 	//Set channel priority
 	switch(Copy_DMA_tTransferConfig->DMA_u8ChannelPriority)
 	{
-		case DMA_PRIORITY_LOW:CLR_BIT(DMA_CCR,PL0);
-							  CLR_BIT(DMA_CCR,PL1);
-							  break;
-		case DMA_PRIORITY_MED:SET_BIT(DMA_CCR,PL0);
-							  CLR_BIT(DMA_CCR,PL1);
-							  break;
-		case DMA_PRIORITY_HIGH:CLR_BIT(DMA_CCR,PL0);
-							   SET_BIT(DMA_CCR,PL1);
-							   break;
-		default: Local_u8ErrorState=NOK;
+	case DMA_PRIORITY_LOW:CLR_BIT(*DMA_CCR,PL0);
+	CLR_BIT(*DMA_CCR,PL1);
+	break;
+	case DMA_PRIORITY_MED:SET_BIT(*DMA_CCR,PL0);
+	CLR_BIT(*DMA_CCR,PL1);
+	break;
+	case DMA_PRIORITY_HIGH:CLR_BIT(*DMA_CCR,PL0);
+	SET_BIT(*DMA_CCR,PL1);
+	break;
+	case DMA_PRIORITY_VERY_HIGH : CLR_BIT(*DMA_CCR,PL0);
+	SET_BIT(*DMA_CCR,PL1);
+	break;
+	default: Local_u8ErrorState=NOK;
 	}
+
+
 	//Set data direction
-	if(Copy_DMA_tTransferConfig->DMA_u8DataDirection == DMA_DIR_MEM2PER)
-		CLR_BIT(DMA_CCR,DIR);
+	if(Copy_DMA_tTransferConfig->DMA_u8DataDirection == DMA_DIR_MEM2MEM)
+	{
+		SET_BIT(*DMA_CCR,MEM2MEM);
+
+	}
 	else if(Copy_DMA_tTransferConfig->DMA_u8DataDirection == DMA_DIR_PER2MEM)
-		SET_BIT(DMA_CCR,DIR);
+	{
+		CLR_BIT(*DMA_CCR,MEM2MEM);
+		CLR_BIT(*DMA_CCR,DIR);
+
+	}
+	else if (Copy_DMA_tTransferConfig ->DMA_u8DataDirection ==DMA_DIR_MEM2PER )
+	{
+		CLR_BIT(*DMA_CCR,MEM2MEM);
+		SET_BIT(*DMA_CCR,DIR);
+
+	}
+
 	else
 		Local_u8ErrorState = NOK;
-	//Set Data Size
-	if(Copy_DMA_tTransferConfig->DMA_u8DataDirection == 8)
+
+
+	//Set Periheral  Data Size
+	if(Copy_DMA_tTransferConfig->DMA_u8PeripheralDataSize == 8)
 	{
-		CLR_BIT(DMA_CCR,MSIZE0);
-		CLR_BIT(DMA_CCR,MSIZE1);
-		CLR_BIT(DMA_CCR,PSIZE0);
-		CLR_BIT(DMA_CCR,PSIZE1);
+		CLR_BIT(*DMA_CCR,PSIZE0);
+		CLR_BIT(*DMA_CCR,PSIZE1);
 	}
-	else if(Copy_DMA_tTransferConfig->DMA_u8DataDirection == 16)
+	else if(Copy_DMA_tTransferConfig->DMA_u8PeripheralDataSize == 16)
 	{
-		SET_BIT(DMA_CCR,MSIZE0);
-		CLR_BIT(DMA_CCR,MSIZE1);
-		SET_BIT(DMA_CCR,PSIZE0);
-		CLR_BIT(DMA_CCR,PSIZE1);
+		SET_BIT(*DMA_CCR,PSIZE0);
+		CLR_BIT(*DMA_CCR,PSIZE1);
 	}
-	else if(Copy_DMA_tTransferConfig->DMA_u8DataDirection == 32)
+	else if(Copy_DMA_tTransferConfig->DMA_u8PeripheralDataSize == 32)
 	{
-		CLR_BIT(DMA_CCR,MSIZE0);
-		SET_BIT(DMA_CCR,MSIZE1);
-		CLR_BIT(DMA_CCR,PSIZE0);
-		SET_BIT(DMA_CCR,PSIZE1);
+		CLR_BIT(*DMA_CCR,PSIZE0);
+		SET_BIT(*DMA_CCR,PSIZE1);
 	}
 	else
 		Local_u8ErrorState = NOK;
+
+	//Set Memory  Data Size
+	if(Copy_DMA_tTransferConfig->DMA_u8MemDataSize == 8)
+	{
+		CLR_BIT(*DMA_CCR,MSIZE0);
+		CLR_BIT(*DMA_CCR,MSIZE1);
+
+	}
+	else if(Copy_DMA_tTransferConfig->DMA_u8MemDataSize == 16)
+	{
+		SET_BIT(*DMA_CCR,MSIZE0);
+		CLR_BIT(*DMA_CCR,MSIZE1);
+
+	}
+	else if(Copy_DMA_tTransferConfig->DMA_u8MemDataSize == 32)
+	{
+		CLR_BIT(*DMA_CCR,MSIZE0);
+		SET_BIT(*DMA_CCR,MSIZE1);
+
+	}
+	else
+		Local_u8ErrorState = NOK;
+
+
 	//Set transfer modes
-	#if DMA_MEM2MEM_MODE == DMA_DISABLE
-	CLR_BIT(DMA_CCR,MEM2MEM);
-	#else
-	SET_BIT(DMA_CCR,MEM2MEM);
-	#endif
-	#if DMA_MEM_INC_MODE == DMA_DISABLE
-	CLR_BIT(DMA_CCR,MINC);
-	#else
-	SET_BIT(DMA_CCR,MINC);
-	#endif
-	#if DMA_PER_INC_MODE == DMA_DISABLE
-	CLR_BIT(DMA_CCR,PINC);
-	#else
-	SET_BIT(DMA_CCR,PINC);
-	#endif
-	#if DMA_CIRCULAR_MODE == DMA_DISABLE
-	CLR_BIT(DMA_CCR,CIRC);
-	#else
-	SET_BIT(DMA_CCR,CIRC);
-	#endif
+#if DMA_MEM2MEM_MODE == DMA_DISABLE
+	CLR_BIT(*DMA_CCR,MEM2MEM);
+#else
+	SET_BIT(*DMA_CCR,MEM2MEM);
+#endif
+#if DMA_MEM_INC_MODE == DMA_DISABLE
+	CLR_BIT(*DMA_CCR,MINC);
+#else
+	SET_BIT(*DMA_CCR,MINC);
+#endif
+#if DMA_PER_INC_MODE == DMA_DISABLE
+	CLR_BIT(*DMA_CCR,PINC);
+#else
+	SET_BIT(*DMA_CCR,PINC);
+#endif
+#if DMA_CIRCULAR_MODE == DMA_DISABLE
+	CLR_BIT(*DMA_CCR,CIRC);
+#else
+	SET_BIT(*DMA_CCR,CIRC);
+#endif
 	//Set Interrupt flag
-	#if DMA_INT_TE == DMA_DISABLE
-	CLR_BIT(DMA_CCR,TEIE);
-	#else
-	SET_BIT(DMA_CCR,TEIE);
-	#endif
-	#if DMA_INT_HT == DMA_DISABLE
-	CLR_BIT(DMA_CCR,HTIE);
-	#else
-	SET_BIT(DMA_CCR,HTIE);
-	#endif
-	#if DMA_INT_TC == DMA_DISABLE
-	CLR_BIT(DMA_CCR,TCIE);
-	#else
-	SET_BIT(DMA_CCR,TCIE);
-	#endif
-	//Enable Channel
-	SET_BIT(DMA_CCR,EN);
-	
+#if DMA_INT_TE == DMA_DISABLE
+	CLR_BIT(*DMA_CCR,TEIE);
+#else
+	SET_BIT(*DMA_CCR,TEIE);
+#endif
+#if DMA_INT_HT == DMA_DISABLE
+	CLR_BIT(*DMA_CCR,HTIE);
+#else
+	SET_BIT(*DMA_CCR,HTIE);
+#endif
+#if DMA_INT_TC == DMA_DISABLE
+	CLR_BIT(*DMA_CCR,TCIE);
+#else
+	SET_BIT(*DMA_CCR,TCIE);
+#endif
+
+	Copy_DMA_tTransferConfig->DMA_Configure = 1 ;
 	return Local_u8ErrorState;
 }
+
+u8 DMA_u8Enable (DMA_t* Copy_DMA_tTransferConfig)
+{
+	u8 Local_u8ErrorState = OK ;
+
+	u32* DMA_CCR;
+
+	DMA_CCR   = (u32*)(DMA1_CCR1   + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET);
+
+	if (Copy_DMA_tTransferConfig->DMA_Configure == 1)
+	{
+		//Enable Channel
+		SET_BIT(*DMA_CCR,EN);
+	}
+	else
+	{
+		Local_u8ErrorState = NOK ;
+
+	}
+	return Local_u8ErrorState ;
+
+
+}
+
+u8 DMA_u8Disable (DMA_t* Copy_DMA_tTransferConfig)
+{
+	u8 Local_u8ErrorState = OK ;
+
+	u32* DMA_CCR;
+
+	DMA_CCR   = (u32*)(DMA1_CCR1   + (Copy_DMA_tTransferConfig->DMA_u8ChannelID-1)*DMA_CHANNEL_OFFSET);
+
+	if (Copy_DMA_tTransferConfig->DMA_Configure == 1)
+	{
+		//Enable Channel
+		CLR_BIT(*DMA_CCR,EN);
+	}
+	else
+	{
+		Local_u8ErrorState = NOK ;
+
+	}
+	return Local_u8ErrorState ;
+
+
+
+
+}
+
