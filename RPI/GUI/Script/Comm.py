8433bcbae5cde47c799fa115e3aa4a7c6a4a5030
Mohamed Hassan <mohamed.hassan2498@gmail.com>
2022-07-08 15:07:18 +0200

    Edited Comm.py to be using GPIO communication.

--- RPI/GUI/Script/Comm.py
+++ RPI/GUI/Script/Comm.py
@@ -1,29 +1,32 @@
-# Functions for communication protocol
-import serial
-import threading
-import time
+import RPi.GPIO as GPIO
 
-global Serial_Port
-Serial_Port = None
+outputUpdate = 8
+outputDiag = 10
+outputResponseFlag = 12
+
+inputUpdate = 16
+inputDiagTemp = 18
+inputDiagDirections = 22
+inputDiagUltra = 24
+inputDiagFlag = 26
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(outputUpdate, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(outputDiag, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(outputResponseFlag, GPIO.OUT, initial=GPIO.HIGH)
+
+GPIO.setup(inputUpdate, GPIO.IN)
+GPIO.setup(inputDiagTemp, GPIO.IN)
+GPIO.setup(inputDiagDirections, GPIO.IN)
+GPIO.setup(inputDiagUltra, GPIO.IN)
+GPIO.setup(inputDiagFlag, GPIO.IN)
 
 
-def Comm_Init():
-    global Serial_Port
-    Serial_Port = serial.Serial("/dev/ttyS0", 9600)
-
-
-def Comm_Read(lenght):
-    global Serial_Port
-    #Serial_Port = serial.Serial("/dev/ttyS0", 9600)
-    data = Serial_Port.read(lenght)
-    return data
-
-
-def Comm_Write(data):
-    global Serial_Port
-    Serial_Port.write(data)
-    
-
-def Comm_Kill_Port():
-    global Serial_Port
-    Serial_Port.close()
+while True:
+    if(GPIO.input(inputUpdate)):
+        GPIO.output(outputUpdate,GPIO.LOW)
+        GPIO.output(outputResponseFlag, GPIO.LOW)
+    else:
+        GPIO.output(outputUpdate, GPIO.HIGH)
+        GPIO.output(outputResponseFlag, GPIO.LOW)
