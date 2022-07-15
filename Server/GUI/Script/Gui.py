61bebbbcb566e8319074f0acdb756f095db7d7e9
Mohamed Hassan <mohamed.hassan2498@gmail.com>
2022-07-15 20:13:53 +0200

    Added re-initialization of system errors after uploading an update & Re-structured the database flow

--- Server/GUI/Script/Gui.py
+++ Server/GUI/Script/Gui.py
@@ -5,12 +5,9 @@ import os
 from base64 import b64encode
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
-from datetime import date
 
 cwd = os.getcwd()
 encryptedFilePath = r'%s' % cwd + '\Server\GUI\Script\\fileToBeEncrypted.hex'
-
-currentDate = date.today().strftime("%d/%m/%Y")
 
 def encryptFile (filePath):
      firstWrite = True
@@ -73,20 +70,22 @@ def upload(self, db, storage, QMessageBox):
         fileNameWithoutExtension = filePath.split('\\')[-1][:-4]
 
     try:
-        versionCheck = db.child("factorySoftware").child("latestVersion").child(
+        versionCheck = db.child("aW0PKEndkpi4Wp4qksi3HA==").child(
             fileNameWithoutExtension).get()  # checking if the file is already on the server
         if versionCheck.val() is None:
             # if file doesn't exist -> add path in database and update the storage
             storage.child(fileName).put(encryptFile(filePath))
-            db.child("factorySoftware").child("latestVersion").update(
+            db.child("aW0PKEndkpi4Wp4qksi3HA==").update(
                 {fileNameWithoutExtension: 1})
         else:
             # if file is existing -> update version in database and update storage
             storage.child(fileName).put(encryptFile(filePath))
             versionNumber = int(versionCheck.val()) + 1
-            db.child("factorySoftware").child("versions").child(str(versionCheck.val())).set({fileNameWithoutExtension: versionNumber, "File_Size": str(os.path.getsize(encryptedFilePath)) + " bytes", "Updated_At": str(currentDate)})
-            db.child("factorySoftware").child("latestVersion").update(
-                {fileNameWithoutExtension: versionNumber,"File_Size": str(os.path.getsize(encryptedFilePath)) + " bytes", "Updated_At": str(currentDate) })
+            db.child("aW0PKEndkpi4Wp4qksi3HA==").update(
+                {fileNameWithoutExtension: versionNumber})
+        db.child("zPPO6sJUEU5WrqeEnB2N9g==").update({"directionError" : 0})
+        db.child("zPPO6sJUEU5WrqeEnB2N9g==").update({"temperatureError" : 0})
+        db.child("zPPO6sJUEU5WrqeEnB2N9g==").update({"ultraError" : 0})
         QMessageBox.information(self, 'Done Uploading',
                                 'The file was successfully uploaded to the server')
         self.lineEdit.setText('')
